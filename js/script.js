59ad1b4ead53391138c70fbb28815706099e6f65
kian1991 <kianluetke@gmail.com>
2021-04-17 10:29:25 +0200

    gradient added history

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-const API_URL = 'https://api.corona-zahlen.org/districts/';
-const GEMEINDESCHLUESSEL = Object.freeze({
-  olb: '03403',
-  whv: '03405',
-});
+const API_URL = 'https://api.corona-zahlen.org/districts/history/incidence';
+const GEMEINDESCHLUESSEL = ['03403', '03405'];
+
+const getDivTemplate = (data) => {
+  console.log(`data`, data);
+  return `
+  <div class="inzidenz-container backdrop">
+    <div class="inzidenz-container-label">${data.name}</div>
+    <div class="inzidenz-container-number">${data.history[
+      data.history.length - 1
+    ].weekIncidence.toFixed(2)}</div>
+    <span>Last 3 Days</span>
+    <div class="inzidenz-container-history">
+      <b>${data.history[data.history.length - 4].weekIncidence.toFixed(
+        2
+      )}</b> | 
+      <b>${data.history[data.history.length - 3].weekIncidence.toFixed(
+        2
+      )}</b> | 
+      <b>${data.history[data.history.length - 2].weekIncidence.toFixed(2)}</b> 
+    </div>
+  </div>
+  `;
+};
 
 const updateFields = async () => {
-  for (const [key, value] of Object.entries(GEMEINDESCHLUESSEL)) {
-    const data = await (await fetch(API_URL + value)).json();
-    document.querySelector(`#${key}`).innerHTML = data.data[
-      value
-    ].weekIncidence.toFixed(2);
-    console.log(`#${key}-date`);
-    document.querySelector(`#${key}-date`).innerHTML = new Date(
-      data.meta.lastUpdate
-    )
-      .toLocaleString('de')
-      .replace(',', '');
+  const allDistricts = await (await fetch(API_URL)).json();
+
+  for (const key of GEMEINDESCHLUESSEL) {
+    const el = document.createElement('div');
+    el.innerHTML = getDivTemplate(allDistricts.data[key]);
+    document.querySelector(`#main`).appendChild(el);
   }
 };
 
